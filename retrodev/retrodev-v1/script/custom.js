27e44723c77d2e8483ce9041b471dd7407702bdd
Jonathan <116063263+Jojogit68@users.noreply.github.com>
2023-03-25 16:43:35 +0100

    Add files via upload

---
+++ retrodev/retrodev-v1/script/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+// Initialisation de la sidebar
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
